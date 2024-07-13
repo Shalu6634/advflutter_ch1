b6a0f5e441e186529239547e4ef253ef75b4ccd9
R4 <sharmashalu3148@>
2024-07-13 12:38:45 +0530

    13-07-24

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
+import 'package:advflutter_ch1/screen/1.4/View/themeView.dart';
+import 'package:advflutter_ch1/screen/1.4/provider/themeprovider.dart';
 import 'package:advflutter_ch1/screen/home/change_theme.dart';
 import 'package:advflutter_ch1/screen/lec-1/Stepper/stepper1.dart';
 import 'package:advflutter_ch1/screen/lec-1/Stepper/stepper2.dart';
-
+import 'package:advflutter_ch1/utils/globalVar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) =>const StepperScreen(),
-        '/stepper': (context) =>const Stepper2(),
-        '/theme': (context) =>const ChangeTheme(),
-      },
+    ChangeNotifierProvider(
+      create: (context) => ChangeProvider(),
+      builder: (context, child) => MaterialApp(
+        theme: ThemeData.dark(),
+        darkTheme: ThemeData.light(),
+        themeMode: isDark ? ThemeMode.light : ThemeMode.dark,
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => ChangeProviderTheme(),
+          // '/stepper1': (context) => StepperScreen(),
+          // '/stepper': (context) => Stepper2(),
+          // '/theme': (context) => ChangeTheme(),
+        },
+      ),
     ),
   );
 }
--- lib/screen/1.4/provider/themeprovider.dart
+++ lib/screen/1.4/provider/themeprovider.dart
@@ -1,1 +1,19 @@
 
+import 'package:flutter/material.dart';
+
+class ChangeProvider extends ChangeNotifier
+{
+  var add = 0;
+  bool isDark = false;
+
+  void changeTheme() {
+    isDark = !isDark;
+    notifyListeners();
+  }
+
+  void addition() {
+    add++;
+    notifyListeners();
+  }
+
+}
