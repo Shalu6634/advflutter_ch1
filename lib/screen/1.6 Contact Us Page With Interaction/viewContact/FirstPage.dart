950e86450e7ea02e7edfbc5a9ef5468e2824bb72
GN <sharmashalu3148@gmail.com>
2024-07-17 06:21:33 +0530

    16-07-24

--- lib/screen/1.6 Contact Us Page With Interaction/viewContact/FirstPage.dart
+++ lib/screen/1.6 Contact Us Page With Interaction/viewContact/FirstPage.dart
@@ -10,7 +10,8 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ContactHomeProvider contactHomeProvider = Provider.of<ContactHomeProvider>(context , listen: false);
+    ContactHomeProvider contactHomeProvider =
+        Provider.of<ContactHomeProvider>(context, listen: false);
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
@@ -38,6 +39,42 @@ class Homepage extends StatelessWidget {
             ),
           ),
           SizedBox(
+            height: height * 0.010,
+          ),
+          Text(
+            textAlign: TextAlign.center,
+            'If you have any queries,get touch with\n us.We will be happy to help you!',
+            style: TextStyle(fontSize: 13),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            height: height * 0.085,
+            width: width * 0.7,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(width: 0.098, color: Colors.grey)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  CupertinoIcons.device_phone_portrait,
+                  color: Colors.blue,
+                  size: 30,
+                ),
+                GestureDetector(
+                    onTap: () {
+                      contactHomeProvider.launcherPhone();
+                    },
+                    child: Text(
+                      '+91 6352413467',
+                      style: TextStyle(fontSize: 16),
+                    )),
+              ],
+            ),
+          ),
+          SizedBox(
             height: 20,
           ),
           Container(
@@ -49,81 +86,90 @@ class Homepage extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Icon(CupertinoIcons.device_phone_portrait,color: Colors.blue,size: 30,),
-                GestureDetector(onTap: (){
-                contactHomeProvider.launcherPhone();
-                },child: Text('+91 6352413467',style: TextStyle(fontSize: 16),)),
+                Icon(
+                  Icons.email_outlined,
+                  color: Colors.blue,
+                  size: 30,
+                ),
+                GestureDetector(
+                    onTap: () {
+                      contactHomeProvider.launcherEmail();
+                    },
+                    child: Text(
+                      'shalu@gmail.com',
+                      style: TextStyle(fontSize: 15),
+                    )),
               ],
             ),
           ),
           SizedBox(
-            height: 30,
+            height: 20,
           ),
           Container(
-            height: height * 0.085,
-            width: width * 0.7,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                border: Border.all(width: 0.098, color: Colors.grey)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(Icons.email_outlined,color: Colors.blue,size: 30,),
-                GestureDetector(onTap: (){
-                  contactHomeProvider.launcherEmail();
-                },child: Text('shalu@gmail.com',style: TextStyle(fontSize: 15),)),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            height: height * 0.5,
+            height: height * 0.4,
             width: width * 0.7,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(5),
                 border: Border.all(width: 0.40, color: Colors.grey)),
             child: Column(
-
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
                   height: height * 0.085,
                   width: width * 0.7,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(2),
-                      border: Border.all(width: 0, color: Colors.grey)),
-                  child: Center(child: Text('Social Media',style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 21),),),
+    ),
+                      // border: Border.all(width: 0.08, color: Colors.grey)),
+                  child: Center(
+                    child: Text(
+                      'Social Media',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 21),
+                    ),
+                  ),
                 ),
                 Container(
-                  height: height * 0.095,
+                  height: height * 0.085,
                   width: width * 0.7,
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(width: 0.090, color: Colors.grey)),
+
+                      border: Border.all(width: 0.030, color: Colors.blue
+                      )
+                  ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Icon(Icons.web,color: Colors.blue,size: 30,),
-                      GestureDetector(onTap: (){
-                        contactHomeProvider.launcherWebsite();
-                      },child: Text('Flutter')),
+                      FlutterLogo(),
+                      GestureDetector(
+                          onTap: () {
+                            contactHomeProvider.launcherWebsite();
+                          },
+                          child: Text('flutter dev')),
                     ],
                   ),
                 ),
                 Container(
-                  height: height * 0.098,
+                  height: height * 0.070,
                   width: width * 0.7,
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(width: 0.090, color: Colors.grey)),
+
+                      // border: Border.all(width: 0.090, color: Colors.grey)
+                    ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Image.asset('assets/img/git.png',height: 30,),
-                      GestureDetector(onTap: (){
-                        contactHomeProvider.launcherSocial();
-                      },child: Text('Github')),
+                      Image.asset(
+                        'assets/img/git.png',
+                        height: 30,
+                      ),
+                      GestureDetector(
+                          onTap: () {
+                            contactHomeProvider.launcherSocial();
+                          },
+                          child: Text('Github')),
                     ],
                   ),
                 ),
@@ -131,15 +177,20 @@ class Homepage extends StatelessWidget {
                   height: height * 0.1,
                   width: width * 0.8,
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
+
                       border: Border.all(width: 0.090, color: Colors.grey)),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Image.asset('assets/img/link.png',height: 40,),
-                      GestureDetector(onTap: (){
-                      contactHomeProvider.launcherSocial1();
-                      },child: Text('Linkedin')),
+                      Image.asset(
+                        'assets/img/link.png',
+                        height: 40,
+                      ),
+                      GestureDetector(
+                          onTap: () {
+                            contactHomeProvider.launcherSocial1();
+                          },
+                          child: Text('Linkedin')),
                     ],
                   ),
                 ),
